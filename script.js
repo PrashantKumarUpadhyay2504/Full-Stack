fc01d9511952c7205cefb0dcda5dea8a83ad9a1e
PrashantKumarUpadhyay2504 <pkumarupadhyay0@gmail.com>
2023-06-16 11:50:39 +0530

    new commit

---
+++ script.js
@@ -0,0 +1,35 @@
+//   Java Script : 
+//  1. To built logical web page .
+
+// Primitive : String , Number , Boolean , undefined , null , Symbol , BigInt.'
+
+var name ='Piyush'
+var num = 123
+console.log(name)
+
+
+// var vs let vs const
+/*
+var : 
+
+adv: we can redeclare and reassign same variable .
+*/
+var name1 = 'gla'
+var name1 = 'Mathura'
+
+/*
+let :
+    1. in let , we can't redeclare but we can reassign with same variable. 
+*/
+
+let num1="jfie"
+num1 = "Mathura"
+
+/*
+const : 
+    1.we can't redeclare and reassign with same variable . 
+*/
+
+const pi=3.14
+
+
