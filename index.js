c102d60fc06782df1064c2d5eca6ace2b08f0164
PrashantKumarUpadhyay2504 <pkumarupadhyay0@gmail.com>
2023-07-05 11:49:28 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// document 
+// console.log(document)
+// let ele= document.getElementsById('id')
+// console.log(ele)
+
+// let ele1 =document.getElementsByClassName('class')
+// console.log(ele2)
+
+// let ele2 =document.getElementsByTagName('h1')
+// console.log(ele3)
+
+
+let ele3 =document.querySelector('.class')
+for(let item of ele){
+    item.style.color='red'
+}
+
+console.log(ele3.innerText)
+
+// console.log(ele4)
+
+// ele3.style.color='red'
+
+
+document.querySelectorAll('h1')
+console.log(ele5)
+
